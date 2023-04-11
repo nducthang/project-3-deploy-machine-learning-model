3085a44d5ff63b39d704d20d6ed7955e9b07252c
nducthang <nguyenducthang8a2@gmail.com>
2023-04-11 23:47:42 +0700

    add test main request

---
+++ test_request.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+data = {"age": 52,
+        "workclass": "Self-emp-inc",
+        "fnlgt": 287927,
+        "education": "HS-grad",
+        "education_num": 9,
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Wife",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 15024,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States"
+        }
+
+response = requests.post(
+    "http://https://udacity-project-3-mlops.onrender.com/predict", data=json.dumps(data))
+
+print(response.status_code)
+print(response.json())
+print(response.json()=={
+    "pred": ">50K"
+})
